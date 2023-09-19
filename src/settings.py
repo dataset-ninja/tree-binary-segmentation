7359fa9910fc845fb907a88791c46db8b46b3e9a
erastov alex <erastov.alex@gmail.com>
2023-09-19 19:30:15 +0300

    set preview image

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/earthshot/tree-binary-segmentation"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 4319385
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/tree-binary-segmentation"
