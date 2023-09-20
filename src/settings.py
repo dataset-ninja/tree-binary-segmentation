2986ae59d4701ed97f59e10c5d276421f6c07035
erastov-alex <erastov.alex@gmail.com>
2023-09-20 13:20:50 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Tree Binary Segmentation"
 PROJECT_NAME_FULL: str = "Tree Binary Segmentation"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
